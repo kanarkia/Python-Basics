e50b899dd4635d1efb93c6f144462917654c543f
kanarkia <adam.kanarkiewicz@atos.net>
2019-06-25 13:36:20 +0200

    My first code line

--- 18_For loop.py
+++ 18_For loop.py
@@ -13,4 +13,8 @@ for my_var3 in range(10):
 for my_var4 in range(len(friends)):
     print(friends[my_var4])
 
-print(len(friends))
+for my_var5 in range(len(friends)):
+    if my_var5 == 0:
+        print('First iteration')
+    else:
+        print('Not first iteration')
