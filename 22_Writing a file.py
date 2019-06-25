c1f616f967459a14560520e4bfb52571c0a34c04
kanarkia <adam.kanarkiewicz@atos.net>
2019-06-25 14:56:49 +0200

    My first code line

---
+++ 22_Writing a file.py
@@ -0,0 +1,14 @@
+employees_var = open('employees.txt', 'a')
+
+employees_var.write('\nLukasz - Tower Head')
+
+employees_var.close()
+
+'''
+employees_var = open('employees2.txt', 'w')
+
+employees_var.write('\nKelly - HR')
+
+employees_var.close()
+'''
+
