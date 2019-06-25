b743ac6e27a364dd1e4036996f543f6e31228172
kanarkia <adam.kanarkiewicz@atos.net>
2019-06-25 13:49:34 +0200

    My first code line

--- 19_Own_function_for_loop.py
+++ 19_Own_function_for_loop.py
@@ -2,7 +2,7 @@ def raise_to_power(base_num, pow_num):
     result = 1
     for var in range(pow_num):
         result = result * base_num
-        print(var)
+        print(var,result)
     return result
 
 print(raise_to_power(2,4))
