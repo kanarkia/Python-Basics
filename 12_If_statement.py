48c110e9a2ea0f65d079a7f1868f960327cef547
kanarkia <adam.kanarkiewicz@atos.net>
2019-06-25 10:47:53 +0200

    My first code line

--- 12_If_statement.py
+++ 12_If_statement.py
@@ -7,6 +7,7 @@ else:
     print('You are a female')
 '''''''''
 
+'''''''''
 is_male = True
 is_tall = True
 
@@ -14,3 +15,16 @@ if is_male or is_tall:
     print('You are a male or tall or both')
 else:
     print('You are NOT a male NOR tall')
+'''''''''
+
+is_male = False
+is_tall = False
+
+if is_male and is_tall:
+    print('You are a male or tall')
+elif is_male and not is_tall:
+    print('You are a male but not tall')
+elif not is_male and is_tall:
+    print('You are not a male but are tall')
+else:
+    print('You are not a male and not tall')
