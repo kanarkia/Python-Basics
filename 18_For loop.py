b01933fae00d4151f42e08da20b4a27c5f1451d9
kanarkia <adam.kanarkiewicz@atos.net>
2019-06-25 13:23:44 +0200

    My first code line

--- 18_For loop.py
+++ 18_For loop.py
@@ -1,6 +1,16 @@
+"""""""""
 for my_var in 'juniper':
     print(my_var)
+"""""""""
 
 friends = ['jim', 'carren', 'kelvin']
 for my_var2 in friends:
     print(my_var2)
+
+for my_var3 in range(10):
+    print(my_var3)
+
+for my_var4 in range(len(friends)):
+    print(friends[my_var4])
+
+print(len(friends))
