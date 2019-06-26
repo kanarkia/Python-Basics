852f97f27f622fae954db00509097a3931704f40
kanarkia <adam.kanarkiewicz@atos.net>
2019-06-26 14:46:41 +0200

    My first code line

---
+++ Python PyEz/Hello-PyEz-v2.py
@@ -0,0 +1,18 @@
+import getpass
+from jnpr.junos import Device
+
+host = None
+uname = None
+pw = None
+
+if host == None:
+    host = input('Hostname or IP:')
+if uname == None:
+    uname = input('Username')
+if pw == None:
+    pw = getpass.getpass()
+
+dev = Device(host=host, user=uname, password=pw)
+dev.open()
+pprint(dev.facts)
+dev.close()
