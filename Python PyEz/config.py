907b65994b0042497c4f5cff1ee2f0867b7987d4
kanarkia <adam.kanarkiewicz@atos.net>
2019-06-26 15:31:21 +0200

    My first code line

---
+++ Python PyEz/config.py
@@ -0,0 +1,17 @@
+from jnpr.junos import Device
+from jnpr.junos.utils.config import Config
+from pprint import pprint
+junos_hosts = ['172.25.11.1']
+
+for ip in junos_hosts:
+    dev = Device(host=ip, user='lab', password='lab123')
+    dev.open()
+    config = Config(dev)
+    config.lock()
+    config.load(path='add_snmp.conf', merge=True)
+    config.pdiff()
+    config.commit()
+    config.unlock()
+    dev.close()
+
+
